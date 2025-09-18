9c3a492ce51af3326019c61c8d2dc555128cf396
Igor Knop <igorknop@gmail.com>
2025-09-18 19:50:01 -0300

    :trash: wipe rep

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/ufjf-dcc207-2024-3-a-atv00-igorknop",
-  plugins: [react()],
-})
